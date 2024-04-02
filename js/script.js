b3d91b7832c225aac90bcf874f029d45d18e83f5
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-04-02 18:28:06 +0000

    added images and js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Nathan De Silva All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Nathan De Silva
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates salary
+ */
+function calculateSalary () {
+  // input
+  const TAX_RATE = 0.18
+  const hoursWorked = parseInt( document.getElementById (hours-worked).value
+  )
+  document.getElementById('salary').innerHTML = `Your pay will be: $ ${takeHomeSalary.toFixed(2)}`
+  document.getElementById('salary').innerHTML = 'Your pay will be: $' + takeHomeSalary.toFixed(2)
+  xx = parseFloat(document.getElementById('salary').value)
+}
